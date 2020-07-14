7bdd9b556aaba08c52cdef7ed896a31469b7dfef
Harry Tran <css012013@coderacademy.edu.au>
2020-07-14 12:28:40 +0000

    add solver isValidSudoku function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy
 import solver
 
 # sample sudoku
-sudoku = [[4,3,5,2,6,9,7,8,1],
+solved = [[4,3,5,2,6,9,7,8,1],
           [6,8,2,5,7,1,4,9,3],
           [1,9,7,8,3,4,5,6,2],
           [8,2,6,1,9,5,3,4,7],
@@ -22,6 +22,16 @@ unsolved = [[5,3,0,0,7,0,0,0,0],
             [0,0,0,4,1,9,0,0,5],
             [0,0,0,0,8,0,0,7,9]]
 
+invalid_unsolved = [[5,3,0,0,7,0,0,0,0],
+                    [6,0,0,1,9,5,0,0,0],
+                    [0,9,8,0,0,0,0,6,0],
+                    [8,0,0,0,6,0,0,0,3],
+                    [4,0,0,8,0,3,0,0,1],
+                    [7,0,0,0,2,0,0,0,6],
+                    [0,6,0,0,0,0,2,8,0],
+                    [0,0,0,4,1,9,0,0,5],
+                    [0,0,0,0,8,0,0,7,7]]
+
 # prints the sudoku to terminal
 def display_board(sudoku):
     print(numpy.matrix(sudoku))
@@ -31,9 +41,17 @@ def display_board(sudoku):
 # tests
 
 # display
-# display_board(sudoku) # terminal display
+# display_board(solved) # terminal display
 # display_board(unsolved) # terminal display
 
 # isValidMove function
-print(solver.isValidMove(unsolved,4,4,5)) # True
-print(solver.isValidMove(unsolved,0,2,5)) # False
+# print(solver.isValidMove(unsolved,4,4,5)) # True
+# print(solver.isValidMove(unsolved,0,2,5)) # False
+# print(solver.isValidMove(solved,0,0,4)) # True
+
+# isValidSudoku function
+# print(solver.isValidSudoku(solved)) # True
+# print(solver.isValidSudoku(unsolved)) # True
+# print(solver.isValidSudoku(invalid_unsolved)) # False
+
+# solve function
--- solver.py
+++ solver.py
@@ -1,12 +1,15 @@
-def isValidMove(sudoku, x, y, num):
+
+
+# checks if there is a duplicate in row, column, 3x3 square
+def isValidMove(sudoku, y, x, num):
     # check row
     for a in range(9):
-        if sudoku[x][a] == num:
+        if sudoku[y][a] == num:
             return False
 
     # check column
     for a in range(9):
-        if sudoku[a][y] == num:
+        if sudoku[a][x] == num:
             return False
 
     # check 3x3 square
@@ -15,8 +18,39 @@ def isValidMove(sudoku, x, y, num):
 
     for a in range(3):
         for b in range(3):
-            if sudoku[fx+a][fy+b] == num:
+            if sudoku[fy+a][fx+b] == num:
                 return False
     
     # does not find duplicate
     return True
+
+
+# checks if sudoku is valid
+def isValidSudoku(sudoku):
+    for a in range(9):
+        for b in range(9):
+            # if cell is not empty and finds a duplicate
+            check_number = sudoku[a][b]
+            if check_number == 0:
+                continue
+
+            sudoku[a][b] = 0
+            if not isValidMove(sudoku, a, b, check_number):
+                sudoku[a][b] = check_number
+                return False
+            sudoku[a][b] = check_number
+
+    # is valid
+    return True
+
+# def solve(sudoku):
+#     for y in range(9):
+#         for x in range(9):
+#             if sudoku[y][x] == 0:
+#                 for num in range(1,10):
+#                     if isValidMove(sudoku, y, x, num):
+#                         sudoku[y][x] = num
+#                         solve(sudoku)
+#                         sudoku[y][x] = 0
+#                     return
+#     return
