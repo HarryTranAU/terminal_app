87c8660563424008ba3eccfbec77d5f3a1220c23
Harry Tran <css012013@coderacademy.edu.au>
2020-07-16 05:04:47 +0000

    add generate to navigation menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy
+import random
 import solver
 
 # Sample sudokus
@@ -42,17 +43,37 @@ test_1 = [[0,0,0,0,0,0,0,2,0],
           [0,0,0,5,0,1,2,0,0],
           [0,1,0,0,0,0,0,0,0]]
 
+blank_board = [[0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0]]
+
 # Messages
 navigation_message = """\nWelcome to my Sudoku App!
 Navigation: Type in the word or number
 1. Solve
-0. Exit\n"""
+2. Generate
+0. Exit
+Input: """
 
 row_message = """\nInput your Sudoku one row at a time, top to bottom
 Enter empty cells as 0 and number cells as the number 1-9
-Type 'back' to remove last row, 'exit' to return to navigation\n"""
+Type 'back' to remove last row, 'exit' to return to navigation
+Input: """
+
+difficulty_message = """\nChoose a difficulty: easy, medium, hard
+Type 'exit' to return to navigation
+Input: """
 
 exit_app = "\nBye!"
+
+
+difficulty = { "easy": 10, "medium": 20, "hard": 30 }
 
 # Prints the sudoku to terminal
 def displayBoard(sudoku):
@@ -61,17 +82,16 @@ def displayBoard(sudoku):
 
 # Takes user input
 def userInputRow():
-    userSudoku = []
-    while len(userSudoku) < 9:
+    user_Sudoku = []
+    while len(user_Sudoku) < 9:
         userRow = input(row_message)
-        # Sanitize input
-        userRow = userRow.replace(" ","")
+        userRow = userRow.replace(" ","").replace(",","") # Remove Spaces and commas
         
         # Check input
         if userRow == "back":
             try:
-                userSudoku.pop()
-                displayBoard(userSudoku)
+                user_Sudoku.pop()
+                displayBoard(user_Sudoku)
                 continue
             except IndexError:
                 print("Nothing to remove, try again\n")
@@ -82,38 +102,68 @@ def userInputRow():
         
         if len(userRow) != 9:
             print("Invalid length, try again\n")
-            displayBoard(userSudoku)
+            displayBoard(user_Sudoku)
         else:
             try:
                 int(userRow)
-                # Convert string to list of int
-                userSudoku.append(list(map(int,userRow)))
-                displayBoard(userSudoku)
+                user_Sudoku.append(list(map(int,userRow))) # Convert string to list of int
+                displayBoard(user_Sudoku)
                 
             except ValueError:
                 print("Invalid input, please use integers from 0-9\n")
 
-    return userSudoku
+    return user_Sudoku
+
+# Generate sudoku
+def generateSudoku(diff):
+    # Create a random valid filled Sudoku
+
+    # Replace cells with 0 according to difficulty
+    return blank_board
 
 
 # Main loop / Navigation
 while True:
     userDecision = input(navigation_message)
-
-    # Sanitise input
-    userDecision = userDecision.lower().replace(" ", "")
+    userDecision = userDecision.lower().replace(" ", "") # Remove spaces and upper case
 
     # 1. Solve
     if userDecision in ["1", "solve"]:
-        userSudoku = userInputRow()
-        if userSudoku == []:
+        user_Sudoku = userInputRow()
+        if user_Sudoku == []: # If user exits solver
             continue
-        elif solver.isValidSudoku(userSudoku):
+        elif solver.isValidSudoku(user_Sudoku): # Checks if user's sudoku is solvable
             print("\nSolution:")
-            solver.solve(userSudoku)
-            displayBoard(userSudoku)
+            solver.solve(user_Sudoku)
+            displayBoard(user_Sudoku)
         else:
             print("Sudoku not valid")
+
+    # 2. Generate
+    if userDecision in ["2", "generate"]:
+        while True:
+            user_difficulty = input(difficulty_message)
+            user_difficulty = user_difficulty.lower().replace(" ", "") # Remove spaces and upper case
+
+            if user_difficulty in difficulty:
+                generatedSudoku = generateSudoku(user_difficulty)
+                displayBoard(generatedSudoku)
+                while True:
+                    want_solution = input("Would you like the solution to this Sudoku? (yes/no)")
+                    want_solution = want_solution.lower().replace(" ", "") # Remove spaces and upper case
+                    if want_solution == "yes":
+                        print("print solution here")
+                        break
+                    elif want_solution == "no":
+                        break
+                
+                # Finish generation and solution
+                break
+            
+            elif user_difficulty == "exit":
+                break
+            else:
+                print("Please choose a difficulty from the list")
 
     # 0. Exit
     if userDecision in ["0", "exit"]:
